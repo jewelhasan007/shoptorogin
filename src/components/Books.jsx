64700ec172f7b58e1336fab4e6bc7356ed8004f4
Jewel Hasan <jewelhasan.eee@gmail.com>
2024-09-16 14:37:11 +0600

    Show All button added

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -4,6 +4,11 @@ import Book from "./Book";
 
 const Books = () => {
     const [books, setBooks] = useState([])
+    const [show, setShow] = useState([3])
+    
+    const handleShowBtn = () =>{
+       setShow(books.length)
+    }
 
     useEffect(()=>{
         fetch('books.json')
@@ -14,8 +19,14 @@ const Books = () => {
         <div >
              <h1 className="text-4xl font-bold text-center my-5">Books </h1>
              <div className="grid md:grid-cols-3">
-             {books.map(book => <Book book={book}></Book>)}
+             {books.slice(0, show).map(book => <Book book={book}></Book>)}
              </div>
+             <div className="text-center">
+             <div className= {show === books.length && 'hidden'}>
+                <button onClick={handleShowBtn} className="btn btn-primary">Show All</button>
+             </div>
+             </div>
+          
              </div>
     );
 };
